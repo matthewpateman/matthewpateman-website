48250ff3f505148e524f75f6c079e709e763d253
matthewpateman <matthewpateman@me.com>
2013-11-29 17:41:56 +0000

    Updated viewport settings and css
    Added margin to footer and header so the article outline doesn’t cover
    them.

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
