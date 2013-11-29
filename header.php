ec45b5e33ca95540789a782167b492aee8fec0b9
matthewpateman <matthewpateman@me.com>
2013-11-29 10:37:17 +0000

    Blog Header Image

--- header.php
+++ header.php
@@ -95,8 +95,32 @@
 	</div>
 </header><!-- #masthead -->
 		<?php 
+
+		if (is_home() )
+		{
+			$args=array(
+			'post_type' => 'page',
+			'pagename' => 'blog'
+  
+);
+$my_query = null;
+$my_query = new WP_Query($args );
+if( $my_query->have_posts() ) {
+  while ($my_query->have_posts()) : $my_query->the_post();
+
+
+$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(320,240), false, '' );
+
+  endwhile;
+}
+wp_reset_query();  // Restore global post data stomped by the_post().
+
+			
+		} else { 
 			//Get the Thumbnail URL
 			$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(320,240), false, '' );
+
+		}
 			if ($src) { 
 		?>
 
