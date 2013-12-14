0b604ea9a83233077d8c96c26d653001ce929431
matthewpateman <matthewpateman@me.com>
2013-12-14 12:12:40 +0000

    updated custom post types

--- functions.php
+++ functions.php
@@ -453,48 +453,6 @@ function twentytwelve_customize_preview_js() {
 }
 add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
 
-add_action( 'init', 'register_cpt_job' );
-
-function register_cpt_job() {
-
-    $labels = array( 
-        'name' => _x( 'Jobs', 'job' ),
-        'singular_name' => _x( 'Job', 'job' ),
-        'add_new' => _x( 'Add New', 'job' ),
-        'add_new_item' => _x( 'Add New Job', 'job' ),
-        'edit_item' => _x( 'Edit Job', 'job' ),
-        'new_item' => _x( 'New Job', 'job' ),
-        'view_item' => _x( 'View Job', 'job' ),
-        'search_items' => _x( 'Search Jobs', 'job' ),
-        'not_found' => _x( 'No jobs found', 'job' ),
-        'not_found_in_trash' => _x( 'No jobs found in Trash', 'job' ),
-        'parent_item_colon' => _x( 'Parent Job:', 'job' ),
-        'menu_name' => _x( 'Jobs', 'job' ),
-    );
-
-    $args = array( 
-        'labels' => $labels,
-        'hierarchical' => true,
-        
-        'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', 'revisions' ),
-        
-        'public' => false,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        
-        
-        'show_in_nav_menus' => false,
-        'publicly_queryable' => true,
-        'exclude_from_search' => true,
-        'has_archive' => true,
-        'query_var' => true,
-        'can_export' => true,
-        'rewrite' => true,
-        'capability_type' => 'page'
-    );
-
-    register_post_type( 'job', $args );
-}
 add_action( 'init', 'register_cpt_experience' );
 
 function register_cpt_experience() {
