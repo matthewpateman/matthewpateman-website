f9642abb4a21837a1e831ae7da25bf01c698e51f
matthewpateman <matthewpateman@me.com>
2014-03-09 12:57:09 +0000

    Article box updates

--- content-article.php
+++ content-article.php
@@ -10,23 +10,23 @@
 
 		<article>
 			<a href="<?php the_permalink(); ?>">
-			<?php 
+			<?php
 				//Get the Thumbnail URL
 				$src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(320,240), false, '' );
-				if ($src) { 
+				if ($src) {
 			?>
 			<img src="<?php echo $src[0] ?>" />
 			<?php } else { ?>
 				<!-- insert template image here -->
 			<?php } ?>
-			<div class="title"><?php the_title(); ?></div>				
+			<div class="title"><?php the_title(); ?></div>
 			<div class="text"><?php the_excerpt(); ?></div>
-			
 
-				
+
+
 			</a>
 			<footer>
 				<div class="date"><?php the_date(); ?></div>
-				<div class="tags"><?php the_tags( '<ul><li>', '</li><li>', '</li></ul>' ); ?> </div>
+				<?php // <div class="tags"><?php the_tags( '<ul><li>', '</li><li>', '</li></ul>' ); ?> </div> ?>
 			</footer>
 		</article>
