57f34eaf3b095ee7abf42903efeb6193af5964cc
matthewpateman <matthewpateman@me.com>
2014-05-26 19:18:16 +0100

    Added Parallax Home-Page
    Not ready for mobile. Update coming shortly

---
+++ content-css.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+<style>
+
+header {
+	width:100%;
+	background: rgba(255,255,255,0.5); /*#E6E6E6;
+	border-bottom:1px solid #DCDCDC; */
+	box-sizing:border-box;
+	overflow: hidden;
+	z-index:500;
+	position: absolute;
+	top:0;
+}
+
+.header .wrapper {display: block;}
+
+.wrapper
+	{
+		width:700px;
+		margin:0 auto;
+		padding:0px 20px;
+		box-sizing: border-box;
+		position: relative;
+	}
+
+#logo {
+	margin: 10px 10px 9px 0px;
+	float: left;
+}
+
+#logo svg {
+	width:100%;
+	height:100%;
+}
+
+#logo svg path {
+	fill:#6D747A;
+}
+
+.header {
+  position: fixed;
+  width:100%;
+height: 70px;
+background: rgba(255,255,255,0.75);
+z-index:4000;
+}
+
+header nav {
+        position: relative;
+        display: -webkit-flex;
+        display: flex; /* or inline-flex */
+        
+        
+        -webkit-align-items: center;
+        align-items: center;
+
+        -webkit-justify-content: center;
+        -webkit-align-items: center;
+
+        width: 100%;
+        margin: 0 auto;
+        height: auto;
+}
+      
+header nav a{
+        flex-grow: 1;
+        padding: 0 10px;
+        font-size: 20px;
+        font-weight: 400;
+        text-decoration: none;
+        line-height: 70px;
+        cursor: pointer;
+      }
+
+	nav {
+		position:absolute;
+		z-index: 600;
+		top:-60px;
+		left:calc(50% - 300px);
+		width:600px;
+
+	}
+
+
+nav ul {
+		margin: -5px 0px 0px 0px;
+		padding: 0px;
+		list-style: none;
+	}
+
+	nav ul li {
+		height: 40px;
+		padding: 0 10px;
+		box-sizing:border-box;
+		font-size: 20px;
+		font-weight: 400;
+		float:left;
+	}
+
+	nav ul li .icon {display: none;}
+	nav ul li span {display: block; float: left; padding:5px;}
+
+#image {
+	width:100%;
+	position: relative;
+	height:200px;
+	margin-top:-89px;
+	}
+
+#image .wrapper {
+	height: 100%;
+}
+
+#image span {
+
+	position:absolute;
+	bottom: 24px;
+	color: #E6E6E6;
+	font-size: 45px;
+	line-height: 42px;
+	letter-spacing: 0px;
+	font-weight: 300;
+}
+
+@media all and (min-width: 481px) {
+
+	header {
+		height: 69px;
+	}
+
+	#logo {
+		width: 80px;
+		height: 50px;
+	}
+
+
+	#menu {display: none;}
+
+}
+
+@media all and (max-width: 480px) {
+
+	#logo {
+		width: 48px;
+		height: 30px;
+	}
+
+	#menu {
+		float:left;
+		font-weight: 600;
+		text-transform: uppercase;
+		padding: 15px 28px 15px 5px;
+		font-size: 12px;
+		background-image: url('data:image/svg+xml;utf8,<svg%20width="30px"%20height="30px"%20xmlns="http://www.w3.org/2000/svg"><polygon%20fill="#6D747A"%20points="9,11%2021,11%2015,18.201%20"/></svg>');
+		background-position: right center;
+		line-height: 20px;
+		background-repeat: no-repeat;
+	}
+
+	.icon {display: none;}
+
+	#grey {
+    width: 100%;
+    height:100%;
+    background:rgba(0,0,0,0.5);
+    position: absolute;
+    z-index:200;
+    top:0;
+  }
+
+	.noscroll {
+    overflow: hidden;
+    position: absolute;
+    top:0;
+    width:100%;
+    height: 100%;
+  }
+
+  #image {
+  	margin-top:-50px;
+  }
+}
+
+</style>
--- page-home.php
+++ page-home.php
@@ -11,80 +11,197 @@
  * @subpackage Twenty_Ten
  * @since Twenty Ten 1.0
  */
-
-get_header(); ?>
-
-	<div id="content">
-<div class="wrapper">
-
-<p>I currently work in marketing and communications. What I like: Gadgets, tech, web, social media, developing and designing...</p>
-
-<p>I have a background in product design and web development. You can find a selection of my past projects here. To find out more click on any of my linked networks and feel free to tweet me <a href="https://twitter.com/intent/tweet?screen_name=matthewpateman">@matthewpateman</a>.</p>
-
-	<h1>Projects</h1>
-		<p><a href="projects">Here are a selection of my projects</a></p>
-</div>
-
-<div class="home-projects articles">
-<?php
-$args=array(
-  
-	'post_type' => 'page',
-	'post_parent' => 6,
-	'posts_per_page' => 3,
-	'sort_order' => 'desc'  
-
-  
-);
-$my_query = null;
-$my_query = new WP_Query($args );
-if( $my_query->have_posts() ) {
-  while ($my_query->have_posts()) : $my_query->the_post(); ?>
-
-
-<?php get_template_part( 'content', 'project' ); ?>
-
-<?php
-  endwhile;
-}
-wp_reset_query();  // Restore global post data stomped by the_post().
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Matthew Pateman</title>
+<meta name="description" content="Personal site of designer Matthew Pateman.">
+<meta name="author" content="Matthew Pateman">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/base.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/main.css">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.png">
+<link rel="apple-touch-icon-precomposed" 					href="<?php echo get_stylesheet_directory_uri() ?>/images/touch-icon-iphone.png" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" 		href="<?php echo get_stylesheet_directory_uri() ?>/images/touch-icon-ipad.png" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" 	href="<?php echo get_stylesheet_directory_uri() ?>/images/touch-icon-iphone-retina.png" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" 	href="<?php echo get_stylesheet_directory_uri() ?>/images/touch-icon-ipad-retina.png" />
+<?php get_template_part( 'content', 'css' ); ?>
 
-<div class="clear"></div>
-</div>
+<style>
+  .header { opacity: 0; }
+</style>
 
-<div class="plain-text wrapper">
-	<h1>Blog</h1>
-	<p>
-		<a href="blog">View my blog</a>
-	</p>
-</div>
+</head>
+<body>
+<div class="container">
+  <div class="container-inner">
+    <div class="background"></div>
 
-<div class="home-projects articles">
-<?php
-$args=array(
+    <?php get_template_part( 'content', 'header' ); ?>
+
+
+
+
+
+    <section id="intro" class="sectionWrapper">
+      <h1 class="name">Matthew Pateman</h1>
+      <h2 class="byline">Designer &amp; Developer</h2>
+      <h2 class="byline2">Digital Products &amp; User Experiences</h2>
+    </section>
+<!--     <section id="testing">
+      <div class="sectionBackground"></div>
+      <h1 class="title">Testing</h1>
+    </section> -->
+    <section id="skills" class="sectionWrapper">
+      <h1 class="skills-title">Skills</h1>
+      <p class="skills-byline">These are some of the things I am great at:</p>
+      <a class="skills-button button" href="http://www.matthewpateman.com/about/">Download CV</a>
+      <ul id="skillsList">
+        <li class="skillsItem skill-1">HTML &amp; CSS<span class="bar scale100"></span></li>
+        <li class="skillsItem skill-2">JavaScript<span class="bar scale80"></span></li>
+        <li class="skillsItem skill-3">PHP<span class="bar scale50"></span></li>
+        <li class="skillsItem skill-4">Ruby on Rails<span class="bar scale50"></span></li>
+        <li class="skillsItem skill-5">Sketching<span class="bar scale80"></span></li>
+        <li class="skillsItem skill-6">Wireframing<span class="bar scale80"></span></li>
+        <li class="skillsItem skill-7">Branding<span class="bar scale80"></span></li>
+        <li class="skillsItem skill-8">Illustrator, InDesign, Photoshop<span class="bar scale100"></span></li>
+        <li class="skillsItem skill-9">Omnigraffle<span class="bar scale90"></span></li>
+        <li class="skillsItem skill-10">Sketch<span class="bar scale60"></span></li>
+      </ul>
+    </section>
+    <section id="projects" class="sectionWrapper">
+      <h1 class="projects-title">Portfolio</h1>
+      <p class="projects-byline">Here's are some of my projects:</p>
+      <a class="projects-button button" href="http://www.matthewpateman.com/projects">View portfolio</a>
+      <ul id="domprojectsList">
+        <li class="dom-projects-item dei-1"></li>
+        <li class="dom-projects-item dei-3"></li>
+        <li class="dom-projects-item dei-9"></li>
+        <li class="dom-projects-item dei-11"></li>
+      </ul>
+    </section>
+    <section id="experience" class="sectionWrapper">
+      <h1 class="experience-title">Experience</h1>
+      <p  class="experience-byline">This is what I am getting up to</p>
+      <a  class="experience-button button" href="http://www.matthewpateman.com/about/">Download CV</a>
+      <div class="experience-line"></div>
+      <ul id="experienceList">
+        <li class="experienceItem experience-1">
+          <span class="time">July 2010<br/>-<br/>Present</span>
+          <span class="dot"></span>
+          <span class="work">American Express</span>
+        </li>
+        <li class="experienceItem detail detail-1">
+          <span>Senior Designer &amp; Manager for Marketing Communications</span>
+          <ul>
+            <li>Led muti-market marketing and communications strategies, plans and platforms for international activities in marketings incl. Canada, Singapore, Hong Kong, Nordics, UK and Australia / New Zealand</li>
+            <li>Managed internal brand strategy and evolution for key initiatives incl. developments and cultural adjustments and adaptation based on consumer insights</li>
+            <li>Led a communications and design team based in London and Singapore</li>
+            <li>Collaborated with international agencies and internal partners to develop campaign creatives</li>
+            <li>Ensured unified aesthetic and consistent content quality across all markets and channels</li>
+            <li>Managed internal stakeholders and project partners involved in programme launches</li>
+          </ul>
+        </li>
+        <li class="experienceItem experience-2">
+          <span class="time">June 2009<br/>-<br/>March 2011</span>
+          <span class="dot"></span>
+          <span class="work">Design Against Crime</span>
+        </li>
+        <li class="experienceItem detail detail-2">
+          <span>Web Designer / Developer &amp; Design Consultant</span>
+            <ul>
+              <li>Developed the relaunch of the research centre’s website with a focus on UX, Social integration, SEO, CMS and an enhanced mobile experience</li>
+              <li>Led the social strategy using Facebook, Twitter, Foursquare and MailChimp and developed Social Apps for the Facebook platform</li>
+              <li>Collaborated in design and user research for multiple brands including French luxury group LVMH</li>
+              <li>Led the design and development of an interactive web-app for Camden Council</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <section id="education" class="sectionWrapper">
+      <h1 class="education-title">Education</h1>
+      <p  class="education-byline">This is what I am getting up to</p>
+      <a  class="education-button button" href="http://www.matthewpateman.com/about/">Download CV</a>
+      <div class="education-line"></div>
+      <ul id="educationList">
+        <li class="educationItem education-1">
+          <span class="time">2009<br/>-<br/>2010</span>
+          <span class="dot"></span>
+          <span class="course">MSc Interactive Multimedia</span>
+        </li>
+        <li class="educationItem detail detail-1"><span>University of Westminster</br>MSc completed with distinction</span>
+          <ul>
+            <li>Final thesis was the user experience of native versus web-app on the iPhone</li>
+            <li>Developing a case study prototype app for the British Museum.</li>
+            <li>Core focus of this course was user experience, user interface design and social media. Attended ‘Future of Web Design’ in London to grow knowledge of trends in the mobile and web space.</li>
+            <li>Designed a conceptual store guide app for Selfridges based on Apple’s iOS design guidelines.</li>
+            <li>Developed an understanding of the agile development process.</li>
+          </ul>
+        </li>
+        <li class="educationItem education-2">
+          <span class="time">2006<br/>-<br/>2009</span>
+          <span class="dot"></span>
+          <span class="course">BA (Hons) Product Design</span>
+        </li>
+        <li class="educationItem detail detail-2"><span>Central Saint Martins College of Art and Design, University of the Arts London<br/>Upper second-class degree</span>
+          <ul>
+            <li>Final project was on preventing product fraud and theft by adding a digital compontent to physical products through integration of RFID and NFC technology (today known as “internet of things”).</li>
+            <li>Developed a working prototype which offered customisation opportunities for brands.</li>
+            <li>Mastered the design process of a product or service from the initial concept through to the final product including preparation for industrial mass production.</li>
+            <li>Worked with multiple brands including Coca Cola South Africa and a conceptual branding project for Audi.</li>
+            <li>Developed an understanding of the design development process and methodology.</li>
+          </ul>
+        </li>
+        <li class="educationItem education-3">
+          <span class="time"><br/>2013<br/></span>
+          <span class="dot"></span>
+          <span class="course">User Experience Design</span>
+        </li>
+        <li class="educationItem detail detail-3">
+          <span>General Assembly London</span>
+          <ul>
+            <li>Successfully completed a 3-month UX course in General Assembly with a seld-initiated UX project.</li>
+          </ul>
+        </li>
+        <li class="educationItem education-4">
+          <span class="time"><br/>2013<br/></span>
+          <span class="dot"></span>
+          <span class="course">Product Management</span>
+        </li>
+        <li class="educationItem detail detail-4">
+           <span>General Assembly London</span>
+          <ul>
+            <li>Developed a business model and working prototype of an online product based on consumer research initiated for this project.</li>
+          </ul>
+        </li>
+        <li class="educationItem education-5">
+          <span class="time"><br/>2012<br/></span>
+          <span class="dot"></span>
+          <span class="course">Backend Web Development</span>
+        </li>
+        <li class="educationItem detail detail-5">
+          <span>General Assembly London</span>
+          <ul>
+            <li>Developed and built a working prototype of a Twitter-like application in Ruby On Rails using GIT and Heroku.</li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+    <section id="connect">
+      <h1 class="connect-byline">Connect with me</h1>
+      <a class="btn button" href="http://linkedin.com/in/matthewpateman" target="_blank">LinkedIn</a>
+      <a class="btn button" href="http://twitter.com/matthewpateman" target="_blank">Twitter</a>
+      <a class="btn button" href="http://github.com/matthewpateman" target="_blank">Github</a>
+      <a class="btn button" href="http://plus.google.com/+matthewpateman" target="_blank">Google+</a>
+    </section>
+  </div>
   
-  'post_type' => 'post',
-  'post_status' => 'publish',
-  'posts_per_page' => 4,
-  'caller_get_posts'=> 1,
-  'order' => DESC,
-  
-);
-$my_query = null;
-$my_query = new WP_Query($args );
-if( $my_query->have_posts() ) {
-  while ($my_query->have_posts()) : $my_query->the_post(); ?>
-
-	<?php get_template_part( 'content', 'article' ); ?>
-
-<?php
-  endwhile;
-}
-wp_reset_query();  // Restore global post data stomped by the_post().
-?>
-	<div class="clear"></div>
-	</div>
 </div>
-
-<?php get_footer(); ?>
+<h3 class="error">Whoops! Right now this demo doesn't handle resizing or browsers less than 1000px wide. Reload this page or get on a laptop!</h3>
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picasso.js"></script>
+</body>
+</html>
